71fb9b2b0f2f9d09d33b198884f8c2087f791447
Kaj Rietberg <kaj@kajrietberg.nl>
2013-11-13 16:22:28 +0100

    Het origineel uploaden
    Signed-off-by: Kaj Rietberg <kaj@kajrietberg.nl>

---
+++ index.php
@@ -0,0 +1,37 @@
+﻿<!DOCTYPE HTML>
+<html lang="nl">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="apple-mobile-web-app-title" content="" />
+    <meta name="msapplication-TileImage" content="windows-icon.png" />
+    <meta name="msapplication-TileColor" content="#4fca96" />
+
+    <title>Home - Project</title>
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <link rel="stylesheet" href="stylesheets/styles.css" />
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="apple-touch-icon-precomposed" href="apple-icon.png" />
+</head>
+
+<body>
+
+    <div class="l-container">
+        <p>Hi, this is Taiga Boilerplate. Find basic modules and examples in <a href="modules.html">modules.html</a></p>
+    </div>
+
+    <!-- Scripts -->
+    <script src="scripts/jquery-2.0.3.min.js"></script>
+    <script src="scripts/interface.js"></script>
+
+</body>
+
+</html>
